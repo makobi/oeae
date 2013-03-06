224f18c96ba16aae7b76075507dc44df68dcbc51
chrisrodz <christian.etpr10@gmail.com>
2013-03-06 14:36:47 -0400

    Added login credentials to no-index.php

--- Front-end/no-index.php
+++ Front-end/no-index.php
@@ -1,3 +1,45 @@
+<?php 
+
+session_start(); 
+
+// Connect to the Database
+$db = mysql_connect("localhost","nosotros","oeaeavaluo2013");
+
+// Choose the database 'Avaluo'
+if ($db) {
+  mysql_select_db("Avaluo");
+} else {
+  echo "no salio";
+  exit();
+}
+
+// Credencials for profesor login
+$email = $_POST['email'];
+$passwd = md5($_POST['passwd']);
+
+// Query to check for credentials
+$loginquery = "SELECT passwd, prof_id, nombre_prof FROM Profesores where email='$email'";
+$credentials = mysql_fetch_array(mysql_query($loginquery));
+
+// If credential are not valid, send back to login page
+if ($credentials['passwd'] !== $passwd) {
+	header('Location: http://ada.uprrp.edu/~chrodriguez/index.php');	
+	exit();
+}
+
+// Save credential in the session variable
+$_SESSION['prof_id'] = $credentials['prof_id'];
+$_SESSION['nombre_prof'] = $credentials['nombre_prof'];
+$_SESSION['email'] = $email;
+
+$query = "SELECT fac_prof, dpto_prof FROM Profesores where prof_id=$_SESSION[prof_id]";
+$prof = mysql_fetch_array(mysql_query($query));
+
+$cursosquery = "SELECT nombre_curso, codificacion FROM Profesores NATURAL JOIN ProfesorImparte natural join Cursos where prof_id=$_SESSION[prof_id]"; 
+$cursos = mysql_query($cursosquery);
+
+?>
+
 <!doctype html> 
 <html lang="en">
 
