ab6e89e4be3eab89a2a4fbae212b5e692d23757c
tahiri-laboy <tahiri.laboy@gmail.com>
2013-04-15 18:20:31 -0400

    Agregado para actividad finalizado También incluyo el archivo de agregados por curso, este está sin implementar en la aplicación aún.

--- Scripts/resultsforact.php
+++ Scripts/resultsforact.php
@@ -22,6 +22,7 @@ if ($link) {
 session_start();
 
 $aid = $_SESSION['act_id'];
+$nombre = $_SESSION['nombre_act'];
 
 /* Se obtiene el logro esperado. */
 $query = mysql_query("SELECT logro_esperado FROM Actividades where act_id='$aid';")
@@ -51,32 +52,36 @@ $query = mysql_query("SELECT distinct mat_id FROM Evaluacion WHERE act_id='$aid'
 	or die(mysql_error());
 $estudiantes = mysql_num_rows($query);
 
-$totales = array(array());
+if ($estudiantes > 0) {
+	$totales = array(array());
 
-/* Se obtienen los totales para cada escala en la rubrica. */
-for($criterio=0; $criterio<$crit_qty; $criterio++) { // Criterios - Filas
-	$totales[$criterio][9] = 0; // Retiene el total de estudiantes con 6 o mas.
+	/* Se obtienen los totales para cada escala en la rubrica. */
+	for($criterio=0; $criterio<$crit_qty; $criterio++) { // Criterios - Filas
+		$totales[$criterio][9] = 0; // Retiene el total de estudiantes con 6 o mas.
 
-	for ($escala=0; $escala<=8; $escala++) { // Escala - Columnas
-    	$query = mysql_query("Select count(ptos_obtenidos) from Evaluacion where 
-			ptos_obtenidos=".$escala." && crit_id=".$cids[$criterio]." 
-			&& act_id=".$aid.";") or die(mysql_error());
-		if(mysql_num_rows($query) > 0) {
-			$result = mysql_fetch_array($query);
-			$totales[$criterio][$escala] = $result[0];
-		} else  $totales[$criterio][$escala] = 0;
-
-		// Se generan los totales de estudiantes que obtuvieron 6 o mas.
-		if ($escala >= 6) {
-			$totales[$criterio][9]+=$totales[$criterio][$escala];
+		for ($escala=0; $escala<=8; $escala++) { // Escala - Columnas
+    		$query = mysql_query("Select count(ptos_obtenidos) from Evaluacion where 
+				ptos_obtenidos=".$escala." && crit_id=".$cids[$criterio]." 
+				&& act_id=".$aid.";") or die(mysql_error());
+			if(mysql_num_rows($query) > 0) {
+				$result = mysql_fetch_array($query);
+				$totales[$criterio][$escala] = $result[0];
+			} else  $totales[$criterio][$escala] = 0;
+	
+			// Se generan los totales de estudiantes que obtuvieron 6 o mas.
+			if ($escala >= 6) {
+				$totales[$criterio][9]+=$totales[$criterio][$escala];
+			}
 		}
-	}
-	// Se halla el porciento de estudiantes que aprobaron el criterio
-	if ($estudiantes > 0) {
+	
 		$totales[$criterio][9] = (($totales[$criterio][9])/$estudiantes)*100;
-	}
-	else
-		$totales[$criterio][9] = 0; 
+
+		// Verificar si el criterio fue aprobado
+		if ($totales[$criterio][9] >= $logroesperado) {
+			$totales[$criterio][10] = 'Aprobado';
+		}
+		else $totales[$criterio][10] = 'No aprobado';
+	} 
 }
 
 //print_r($totales);
@@ -121,7 +126,7 @@ echo '					<table id="thumb0">
 				</tr>
 				</table>';
 echo "
-	<h3>Resultados para la actividad ".$aid."</h3>";
+	<h3>Resultados para ".$nombre."</h3>";
 echo "  <p> Logro esperado: ".$logroesperado."\n";
 
 $tabla = "";
@@ -129,40 +134,136 @@ $tabla = "";
 /* Si se han evaluado estudiantes, desplegamos la tabla */
 if ($estudiantes != 0) {
   // Se comienza a generar la tabla de los totales
-  $tabla.="<table id = totales>
-	 <caption>Tabla de distribución de puntuaciones</caption>
-            <tr>
-	       <th>Criterios</th>
-	       <th colspan='9'>Escala</th>
-	       <th>Porciento de estudiantes que aprobaron</th>
-	    </tr>
-	    <tr><td> </td>
-		<td>0</td>
-		<td>1</td>
-		<td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-		<td>8</td>
-		<td> </td> 
+  $tabla.="<table id = grading>
+	 <caption><h4>Tabla de distribución de puntuaciones por criterio</h4></br>
+	</caption>
+	       <thead><td><p>Criterios</p></td>
+	       <td colspan='9'><p>Escala</p></td>
+	       <td><p>Porciento de estudiantes que aprobaron</p></td>
+		   <td><p>Aprobado o no aprobado</p></td></thead>
+	    <tbody><tr><td> </td>
+				<td><b>0</b></td>
+				<td><b>1</b></td>
+				<td><b>2</b></td>
+                <td><b>3</b></td>
+                <td><b>4</b></td>
+                <td><b>5</b></td>
+                <td><b>6</b></td>
+                <td><b>7</b></td>
+				<td><b>8</b></td>
+				<td> </td> 
             </tr>";
 
   // Se llena la tabla con los resultados
   for ($i=0; $i<$crit_qty; $i++) {
     $tabla.="<tr><td><p>".$criterios[$i]."</p>";
-    for($j=0; $j<=9; $j++) {
+    for($j=0; $j<=10; $j++) {
 	$tabla.="</td>
-                 <td>".$totales[$i][$j]."</td>";
+                 <td><p>".$totales[$i][$j]."</p></td>";
     }
     $tabla.="</tr>";
   }
 
-  $tabla.="</table>";
+  $tabla.="</tbody></table></br></br>";
+
+/***************************** AGREGADOS POR DOMINIOS *****************************/
+
+// Obtengo los nombres de los diferentes dominios
+$query = mysql_query("Select distinct nombre_dom, nombre_crit from Dominios natural join 
+	Criterios natural join Evaluacion natural join CriterioPertenece where 	
+	act_id='$aid';") or 	
+	die(mysql_error());
+
+$dominios = array();	// Nombres de los dominios
+$d_span = array();		/* Span de cada dominio (para efecto rowspan en tabla html) -
+						   Almacena la cantidad de criterios asociados al dominio */
+$criterios2 = array();	/* Nombres de los criterios (esta vez en el orden asociado 
+						   a los dominios) */
+$i = -1;	
+
+while ($result = mysql_fetch_array($query)) {
+	
+	$criterios2[] = $result['nombre_crit'];
+
+	// Si el dominio no esta ya en el arreglo, se guarda
+	if (!in_array($result['nombre_dom'], $dominios)) { 
+		$dominios[] = $result['nombre_dom'];
+		$i++;	
+		$d_span[$i] = 1;
+	}
+	else {
+		$d_span[$i]++;
+	}
+}
+
+$aprobados = array(); // Acumulador de criterios aprobados por dominio.
+for ($i = 0; $i < sizeof($dominios); $i++) $aprobados[$i] = 0; // Inicializacion
+
+$porciento = array(); // Almacena el porciento de criterios aprobados
+$dom_alcanzado = array(); // Retiene si se alcanzo o no el dominio
+
+$first = 0; // Donde se comienza a iterar 
+$last = 0;  // Donde se termina de iterar
+$iter = 0;  // Contador de iteraciones - Cada iteracion corresponde a un dominio
+
+foreach ($d_span as $asociados) {
+	$last += $asociados; /* Termina cuando se itere por todos los criterios
+							asociados al dominio. */
+	
+	/* Se verifica cuantos de los criterios alineados a los dominios fueron aprobados. */
+	for ($i = $first; $i < $last; $i++) {
+		$fila = array_search($criterios2[$i], $criterios);
+		if ($totales[$fila][10] == 'Aprobado') {
+			$aprobados[$iter]++; // Aumento el conteo de criterios aprobados
+		}
+	}
+	// Se computa el porciento de criterios aprobados
+	$porciento[] = round(($aprobados[$iter]/$asociados)*100,2);
+
+	// Se verifica si el dominio se alcanzo
+	if (round($porciento[$iter],-1) >= 70) {
+		$dom_alcanzado[] = 'Aprobado';
+	}
+	else $dom_alcanzado[] = 'No aprobado';
+
+	$first = $last; // Establezco desde donde comienza la nueva iteracion.
+	$iter++; // Vamos al proximo dominio
+}
+
+//print_r($aprobados); 
+
+$tabla.="<table id = grading>
+	 <caption><h4>Tabla de agregados por dominios</h4></br></caption>
+        <thead>
+	       <td><p>Dominios</p></td>
+	       <td><p>Criterios Alineados</p></td>
+	       <td><p>Criterios Aprobados</p></td>
+		   <td><p>Porcentaje</p></td>
+		   <td><p>Aprobado o no aprobado</p></td>
+	    </thead><tbody>";
+
+$dom_i = 0;
+$span = 0;
+
+for ($i=0; $i<sizeof($criterios2); $i++) {
+	if($i == $span) {
+		$tabla.="<tr><td rowspan=".$d_span[$dom_i].">".$dominios[$dom_i]."</td>";
+	}
+    $tabla.="<td>".$criterios2[$i]."</td>";
+	if($i == $span) {
+		$tabla.="<td rowspan=".$d_span[$dom_i].">".$aprobados[$dom_i]." de ".$d_span[$dom_i]." </td>";
+		$tabla.="<td rowspan=".$d_span[$dom_i].">".$porciento[$dom_i]."</td> 
+		<td rowspan=".$d_span[$dom_i].">".$dom_alcanzado[$dom_i]."</td>" ;
+		$span += $d_span[$dom_i];
+		$dom_i++;
+	}
+	$tabla.="</tr>";
+}
+$tabla.="</tbody></table>";
+ 
 }
 else { // No se han evaluado estudiantes
-  $tabla.="Error: No se han realizado evaluaciones.";
+  $tabla.="<p> Error: Aún no se han realizado evaluaciones para esta actividad.";
 }   
   $tabla.="</center></div>";
 
